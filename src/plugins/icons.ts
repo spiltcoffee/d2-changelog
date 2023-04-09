8dd4c2bb8f6dcdd8abe1bc545dff26d0dd71718d
spiltcoffee <spiltcoffee@users.noreply.github.com>
2023-04-09 23:13:22 +1000

    fix: fix default icons.ts export

--- src/plugins/icons.ts
+++ src/plugins/icons.ts
@@ -2,7 +2,7 @@ import type { App } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
-export async function loadIcons(app: App) {
+export default async function loadIcons(app: App) {
   // library.add(faSomeIcon)
 
   app.component("fa-icon", FontAwesomeIcon);
