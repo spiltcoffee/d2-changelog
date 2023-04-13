2f43e99a5fca3d3df034d5e103aa730ebe3768c7
spiltcoffee <spiltcoffee@users.noreply.github.com>
2023-04-13 14:28:03 +1000

    build: allow publicPath to be set by env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import process from "node:process";
 import vue from "@vitejs/plugin-vue";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
@@ -15,4 +16,5 @@ export default defineConfig({
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: { port: 3000 },
+  base: process.env.D2_CHANGELOG_PATH || "/",
 });
